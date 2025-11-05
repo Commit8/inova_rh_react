8bbf93715acf7cfb8fc3f1d0917badfce7e418ee
L4ish <laish_rodrigues@hotmail.com>
2025-11-05 15:23:58 -0300

    ajustes da home: bg e botão

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,22 +3,22 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <>
-      <div className="bg-[linear-gradient(137deg,#00A597_0%,#ffffff_50%)] flex justify-center h-screen font-inter">
+      <div className="bg-[linear-gradient(130deg,#00A597_0%,#f3f5f6_80%)] flex justify-center h-screen font-inter bg-cover bg-no-repeat">
         <div className="grid grid-cols-2 w-full h-full text-black">
           <div className="flex flex-col gap-4 items-center justify-center p-15">
-            <h2 className="text-5xl font-bold text-shadow-lg/17">Simplifique a gestão de pessoas na sua <span className="text-[#145764]">empresa</span> </h2>
+            <h2 className="text-6xl font-bold text-shadow-lg/17">Simplifique a gestão de pessoas na sua <span className="text-[#f3f5f6]">empresa</span> </h2>
 
-            <p className="text-xl mt-8">
+            <p className="text-2xl mt-8">
               O InovaRH é uma plataforma prática e intuitiva para gerenciar colaboradores e otimizar processos de Recursos Humanos.
             </p>
 
-            <div className="flex justify-around gap-4 mt-8">
-              <button className=" bg-[#031D40] text-white px-4 py-2 hover:bg-[#145764] rounded-full shadow-lg shadow-[#031D40]">
+            <div className="flex justify-around gap-4 mt-4">
+              <button className="border-2 bg-[#f3f5f6] text-[#031D40] px-4 py-3 hover:text-[#f3f5f6] hover:bg-[#145764] rounded-full shadow-lg shadow-[#031D40] text-xl">
                     <Link to='/produto'>Quero saber mais!</Link> 
                 </button>
             </div>
           </div>
-          <div className="flex items-center flex-col justify-center bg-white">
+          <div className="flex items-center flex-col justify-center">
             <img
               src="https://i.imgur.com/nKl4Nsh.png"
               alt="Imagem Página Home"
