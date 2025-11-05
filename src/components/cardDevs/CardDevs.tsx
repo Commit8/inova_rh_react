f0d35719ebdd38c3b809172505ed5740771fc8ec
Akanni Silva <akanni029@gmail.com>
2025-11-05 14:24:34 -0300

    correções da page sobre nos

--- src/components/cardDevs/CardDevs.tsx
+++ src/components/cardDevs/CardDevs.tsx
@@ -24,8 +24,8 @@ function CardDevs(cardDev: CardPDevsProps) {
           </div>
           <div>
             {" "}
-            <h2 className="text-3xl font-inter font-bold">{cardDev.nome}</h2>
-            <h3 className="text-3xl font-inter font-bold" >Dev.</h3>
+            <h2 className="text-2xl font-inter font-bold  ">{cardDev.nome}</h2>
+            <h3 className="text-3xl font-inter font-bold">Dev.</h3>
             <p>{cardDev.descricao}</p>
             <a href={cardDev.link} target="_blank">
               <GithubLogoIcon
@@ -47,8 +47,8 @@ function CardDevs(cardDev: CardPDevsProps) {
           </div>
           <div>
             {" "}
-            <h2 className="text-3xl font-inter font-bold">{cardDev.nome}</h2>
-            <h3 className="text-3xl font-inter font-bold" >Dev.</h3>
+            <h2 className="text-2xl font-inter font-bold ">{cardDev.nome}</h2>
+            <h3 className="text-3xl font-inter font-bold">Dev.</h3>
             <p>{cardDev.descricao}</p>
             <a href={cardDev.link} target="_blank">
               <GithubLogoIcon
@@ -70,7 +70,7 @@ function CardDevs(cardDev: CardPDevsProps) {
           </div>
           <div>
             {" "}
-            <h2 className="text-3xl font-inter font-bold">{cardDev.nome}</h2>
+            <h2 className="text-2xl font-inter font-bold  ">{cardDev.nome}</h2>
             <h3 className="text-3xl font-inter font-bold">Dev.</h3>
             <p>{cardDev.descricao}</p>
             <a href={cardDev.link} target="_blank">
@@ -89,11 +89,15 @@ function CardDevs(cardDev: CardPDevsProps) {
       component = (
         <div className="bg-[#01B18B] flex lg:flex-col rounded-3xl justify-center items-center mx-4 text-white my-6 p-6 text-center gap-2">
           <div>
-            <img src={cardDev.foto} className="rounded-full pb-3" alt="Foto do Dev" />
+            <img
+              src={cardDev.foto}
+              className="rounded-full pb-3"
+              alt="Foto do Dev"
+            />
           </div>
           <div>
             {" "}
-            <h2 className="text-3xl font-inter font-bold" >{cardDev.nome}</h2>
+            <h2 className="text-2xl font-inter font-bold  ">{cardDev.nome}</h2>
             <h3 className="text-3xl font-inter font-bold">Dev.</h3>
             <p>{cardDev.descricao}</p>
             <a href={cardDev.link} target="_blank">
